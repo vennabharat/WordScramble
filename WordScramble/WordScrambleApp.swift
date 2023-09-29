cbd546922995cf2e381f30b4569f852ba61c1bfc
Venna Bharat <v.bharath39@gmail.com>
2023-09-29 06:14:24 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by bharat venna on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
